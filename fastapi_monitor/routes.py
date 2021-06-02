62be60e2f2208425456fd206baadb4cfdd700eae
long2ice <long2ice@gmail.com>
2021-06-02 23:12:01 +0800

    update pages

--- fastapi_monitor/routes.py
+++ fastapi_monitor/routes.py
@@ -9,10 +9,16 @@ router = APIRouter()
 
 @router.get("/")
 async def monitor(request: Request):
-    return templates.TemplateResponse("index.html", context={"request": request})
+    return templates.TemplateResponse("dashboard.html", context={"request": request})
 
 
 @router.get("/logs")
-async def logs(request: Request):
-    data = await Log.all()
-    return templates.TemplateResponse("index.html", context={"request": request, 'logs': data})
+async def logs_page(request: Request):
+    return templates.TemplateResponse("logs.html", context={"request": request})
+
+
+@router.get("/logs/data")
+async def logs_data(request: Request, limit: int, offset: int):
+    total = await Log.all().count()
+    data = await Log.all().limit(limit).offset(offset)
+    return {'total': total, 'rows': data}
